5efaac43661fb6086b07e9c39840e04099921be7
Thomasuzan <thomasuzanpro@gmail.com>
2025-11-13 20:29:23 +0100

    config: prepare project for Render deployment

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,18 @@ const { Sequelize } = require('sequelize');
 
 // On créé une connexion à la bdd SQLite
 const sequelize = new Sequelize({
-    dialect: 'sqlite', // Type de bdd
-    storage: './database.sqlite', // Où sera stockée la BDD
-    logging: false // Pour ne pas afficher les requêtes SQL dans la console
+
+    // En développement (local), on utilise ./database.sqlite
+    // dialect: 'sqlite', // Type de bdd
+    // storage: './database.sqlite', // Où sera stockée la BDD
+    // logging: false // Pour ne pas afficher les requêtes SQL dans la console
+
+    // En production (Render), on utilise /tmp pour le stockage temporaire
+    dialect: 'sqlite',
+    storage: process.env.NODE_ENV === 'production' 
+      ? '/tmp/database.sqlite' 
+      : './database.sqlite',
+    logging: false    
 });
 
 // Fonction pour tester la connexion
